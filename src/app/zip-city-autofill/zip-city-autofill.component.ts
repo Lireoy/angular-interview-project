4991e990b8588379e8dd2d612173507a85ad399c
Lireoy <34489456+Lireoy@users.noreply.github.com>
2020-08-18 17:14:14 +0200

    City autofill just works with zip codes.

--- src/app/zip-city-autofill/zip-city-autofill.component.ts
+++ src/app/zip-city-autofill/zip-city-autofill.component.ts
@@ -1,4 +1,11 @@
 import {Component} from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+
+/*
+interface City {
+  zip: number;
+  city: string;
+}*/
 
 @Component({
   selector: 'app-zip-city-autofill',
@@ -7,5 +14,29 @@ import {Component} from '@angular/core';
 })
 export class ZipCityAutofillComponent {
 
+  zipCityMap = new Map<string, string>()
+    .set('4288', 'Újléta')
+    .set('4024', 'Debrecen')
+    .set('1007', 'Budapest')
+    .set('8900', 'Zalaegerszeg')
+    .set('9024', 'Győr');
+
+  zipInput = new FormControl('', [
+    Validators.required,
+    Validators.pattern('[0-9]*'),
+    Validators.minLength(4),
+    Validators.maxLength(4)
+  ]);
+
+  cityOutput = new FormControl(this.zipCityMap.get(this.zipInput.value));
+
+  /*
+  zips: City[] = [
+    {zip: 4024, city: 'Debrecen'},
+    {zip: 1007, city: 'Budapest'},
+    {zip: 8900, city: 'Zalaegerszeg'},
+    {zip: 9023, city: 'Győr'},
+    {zip: 4288, city: 'Újléta'}
+  ];*/
 
 }
