dfbfbad7d8676317333011d24476369cfb901072
Lireoy <34489456+Lireoy@users.noreply.github.com>
2020-08-20 13:40:02 +0200

    Remove unused and duplicate code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,27 +11,15 @@ import {MatListModule} from '@angular/material/list';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
-import {EmailValidatorComponent} from './email-validator/email-validator.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
-import {NameValidatorComponent} from './name-validator/name-validator.component';
-import {PhoneValidatorComponent} from './phone-validator/phone-validator.component';
-import {CountryValidatorComponent} from './country-validator/country-validator.component';
 import {MatSelectModule} from '@angular/material/select';
-import { ZipCityAutofillComponent } from './zip-city-autofill/zip-city-autofill.component';
-import { BillingAddressComponent } from './billing-address/billing-address.component';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShippingAddressComponent,
-    EmailValidatorComponent,
-    NameValidatorComponent,
-    PhoneValidatorComponent,
-    CountryValidatorComponent,
-    ZipCityAutofillComponent,
-    BillingAddressComponent,
+    ShippingAddressComponent
   ],
   imports: [
     BrowserModule,
